996b7292ed3a1f5af84c96238ba1d4288c2ca40c
yapurambharath <yapurambharath@gmail.com>
2020-07-28 12:13:16 +0530

    committing recent updates

--- src/com/project/screens/Google.java
+++ src/com/project/screens/Google.java
@@ -13,14 +13,16 @@ import com.project.utilities.ExcelUtil;
 public class Google extends DriverClass {
 	CommonMethods objCM = new CommonMethods();
 	String xlFile = ".//TestData//TestData.xlsx";
-	String xlSheet = "Games";
+	String[] xlSheet = {"Games","Movies"};
 
 	public void imageTest() throws IOException {
 		test = extent.createTest("This is testing Images");
-		int n = ExcelUtil.rowCount(xlFile, xlSheet);
+		for(int j=0;j<2;j++)
+		{
+		int n = ExcelUtil.rowCount(xlFile, xlSheet[j]);
 		for (int i = 1; i <= n; i++) {
 			driver.findElement(GooObj.objImg).click();
-			String value = ExcelUtil.getCellData(xlFile, xlSheet, i, 1);
+			String value = ExcelUtil.getCellData(xlFile, xlSheet[j], i, 1);
 			driver.findElement(GooObj.objSearch).sendKeys(value);
 			driver.switchTo().activeElement().sendKeys(Keys.ENTER);
 			String url = driver.getCurrentUrl();
@@ -28,15 +30,15 @@ public class Google extends DriverClass {
 				test.log(Status.PASS, "Images are displayed Successfully");
 				objCM.takeScreenshot(value);
 				driver.navigate().to("https://www.google.com");
-				ExcelUtil.setCellData(xlFile, xlSheet, i, 2, "PASSED");
+				ExcelUtil.setCellData(xlFile, xlSheet[j], i, 2, "PASSED");
 				if(value.toLowerCase().contains("prince"))
 				{
-					ExcelUtil.setCellData(xlFile, xlSheet, i, 2, "FAILED");
+					ExcelUtil.setCellData(xlFile, xlSheet[j], i, 2, "FAILED");
 				}
 			} else {
 				test.log(Status.FAIL, "Images not displayed");
 			}
-
+		}
 		}
 
 	}
