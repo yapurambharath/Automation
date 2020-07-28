c276d252fee09008bba36ddb1222852e5f281547
yapurambharath <yapurambharath@gmail.com>
2020-07-28 12:03:41 +0530

    Committing changes

--- src/com/project/screens/Google.java
+++ src/com/project/screens/Google.java
@@ -8,25 +8,36 @@ import com.aventstack.extentreports.Status;
 import com.project.objects.GooObj;
 import com.project.utilities.CommonMethods;
 import com.project.utilities.DriverClass;
+import com.project.utilities.ExcelUtil;
 
-public class Google extends DriverClass{
-	CommonMethods objCM=new CommonMethods();
-	public void imageTest() throws IOException
-	{
-		test=extent.createTest("This is testing Images");
-		driver.findElement(GooObj.objImg).click();
-		driver.findElement(GooObj.objSearch).sendKeys("AC Brotherhood");
-		driver.switchTo().activeElement().sendKeys(Keys.ENTER);
-		String url=driver.getCurrentUrl();
-		if(url.toLowerCase().contains("brotherhood"))
-		{
-			test.log(Status.PASS, "Images are displayed Successfully");
-			driver.navigate().to("https://www.google.com");
-			objCM.takeScreenshot("images");
+public class Google extends DriverClass {
+	CommonMethods objCM = new CommonMethods();
+	String xlFile = ".//TestData//TestData.xlsx";
+	String xlSheet = "Games";
+
+	public void imageTest() throws IOException {
+		test = extent.createTest("This is testing Images");
+		int n = ExcelUtil.rowCount(xlFile, xlSheet);
+		for (int i = 1; i <= n; i++) {
+			driver.findElement(GooObj.objImg).click();
+			String value = ExcelUtil.getCellData(xlFile, xlSheet, i, 1);
+			driver.findElement(GooObj.objSearch).sendKeys(value);
+			driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+			String url = driver.getCurrentUrl();
+			if (url.toLowerCase().contains("img")) {
+				test.log(Status.PASS, "Images are displayed Successfully");
+				objCM.takeScreenshot(value);
+				driver.navigate().to("https://www.google.com");
+				ExcelUtil.setCellData(xlFile, xlSheet, i, 2, "PASSED");
+				if(value.toLowerCase().contains("prince"))
+				{
+					ExcelUtil.setCellData(xlFile, xlSheet, i, 2, "FAILED");
+				}
+			} else {
+				test.log(Status.FAIL, "Images not displayed");
+			}
+
 		}
-		else
-		{
-			test.log(Status.FAIL, "Images not displayed");
-		}
+
 	}
 }
--- src/com/project/utilities/ExcelUtil.java
+++ src/com/project/utilities/ExcelUtil.java
@@ -1,5 +1,89 @@
 package com.project.utilities;
 
-public class ExcelUtil {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+public class ExcelUtil {
+	public static FileInputStream fi;
+	public static Workbook wb;
+	public static Sheet sh;
+	public static Row ro;
+	public static Cell ce;
+	public static FileOutputStream fo;
+	public static CellStyle style;
+
+	public static int rowCount(String xlFile, String xlSheet) throws IOException {
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		sh = wb.getSheet(xlSheet);
+		int rowCount = sh.getLastRowNum();
+		fi.close();
+		wb.close();
+		return rowCount;
+	}
+
+	public static int ColCount(String xlFile, String xlSheet, int rowNum) throws IOException {
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		sh = wb.getSheet(xlSheet);
+		ro = sh.getRow(rowNum);
+		int colCount = ro.getLastCellNum();
+		fi.close();
+		wb.close();
+		return colCount;
+	}
+
+	public static String getCellData(String xlFile, String xlSheet, int rowNum, int colNum) throws IOException {
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		sh = wb.getSheet(xlSheet);
+		ro = sh.getRow(rowNum);
+		ce = ro.getCell(colNum);
+		String data;
+		try {
+			data = ce.getStringCellValue();
+		} catch (Exception e) {
+			data = "";
+		}
+		fi.close();
+		wb.close();
+		return data;
+	}
+
+	public static void setCellData(String xlFile, String xlSheet, int rowNum, int colNum,String data) throws IOException{
+		fi = new FileInputStream(xlFile);
+		wb = new XSSFWorkbook(fi);
+		sh = wb.getSheet(xlSheet);
+		ro = sh.getRow(rowNum);
+		ce=ro.createCell(colNum);
+		ce.setCellValue(data);
+		if(data=="PASSED")
+		{
+			style=wb.createCellStyle();
+			style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+			style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			ce.setCellStyle(style);
+		}
+		else
+		{
+			style=wb.createCellStyle();
+			style.setFillForegroundColor(IndexedColors.RED.getIndex());
+			style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+			ce.setCellStyle(style);
+		}
+		fo=new FileOutputStream(xlFile);
+		wb.write(fo);
+		wb.close();
+		fi.close();	
+		fo.close();
+	}
 }
